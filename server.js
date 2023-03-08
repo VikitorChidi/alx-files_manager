e1280e4d243b4dd30db2b5f0c001df5c4ab91a1e
VikitorChidi <victorblessed9@gmail.com>
2023-03-08 01:30:32 +0100

    new task

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const routes = require('./routes/index');
+const routes = require("./routes/index");
 
 app.use(express.json());
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
