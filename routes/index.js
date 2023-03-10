51b3a100fb3f3dd6a52a8527f706bc3f20a2737b
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2023-03-10 19:03:01 +0100

    updating

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,24 @@
-import express from "express";
-import AppController from "../controllers/AppController";
-import UsersController from "../controllers/UsersController";
+import express from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
-router.get("/status", AppController.getStatus);
-router.get("/stats", AppController.getStats);
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
-router.post("/users", UsersController.postNew);
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
 
-module.exports = router;
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/publish', FilesController.putUnpublish);
+
+export default router;
